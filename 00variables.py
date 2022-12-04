f083cc5e123f4d4f1375debd5ceebd4043129b3a
Jose <jose.manuel@holafly.com>
2022-12-04 12:22:00 +0100

    first approach to variables

---
+++ 00variables.py
@@ -0,0 +1,45 @@
+## Convencion
+# snake_case
+my_name_is_jose = 'whatever u want'
+PI = 3.14
+
+# string 
+hello = 'hello world'
+print(hello)
+
+# Numbers
+
+first_number = 1
+second_number = 1.5
+big_number = 50_000_000
+print(big_number)
+
+# Booleans
+
+True
+False
+
+## Complex data
+
+# Lists
+shopping = ['banana', 'milk', 'chocolate']
+             # 0         1        2
+print(shopping)
+shopping[0] = 'apples'
+print(shopping)
+
+# Tuplas
+wheels = (4, 2)
+print(wheels)
+# print(wheels[1])
+# wheels[1] = 7
+
+# 
+second_a = {'Adrian', 'Emma', 'Gabriel', 'Adrian'}
+print(second_a)
+
+# Dictionaries
+person = {'name': 'Jose', 'age':39, 'handsome':True,
+ 'programming_languages':['Python', 'Javascript']}
+
+print(person)
